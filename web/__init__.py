cc2a57bd6cf8d797508057239406b383cbd563d0
rsirimalla <ramesh.sirimalla@gmail.com>
2019-09-24 15:58:27 -0400

    response aggregation

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
+import requests
+import socket
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from ' + IPAddr
 
 
 @app.route('/user')
@@ -15,3 +19,11 @@ def user():
         email='axl@darknet.com',
         id=666
     )
+
+
+@app.route('/aggregate')
+def aggregate():
+    r1 = requests.get('http://localhost:8080/user')
+    r2 = requests.get('http://localhost:8080/user')
+    r3 = requests.get('http://localhost:8080/user')
+    return jsonify(r1.json(), r2.json(), r3.json())
