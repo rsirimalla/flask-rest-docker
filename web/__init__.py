9a7bf31fa7aaa8b92b5526a92a7f7917e90f5363
rsirimalla <ramesh.sirimalla@gmail.com>
2019-10-02 15:28:29 -0400

    Request forwarding

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import socket
 import os
@@ -7,21 +7,25 @@ app = Flask(__name__)
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 
-service_endpoint = os.environ['SERVICE_ENDPOINT']
+service_endpoint = os.environ.get('SERVICE_ENDPOINT')
 
 
 @app.route('/')
 def hello_world():
     return jsonify(
-        message='Hello world',
+        message='Hello World',
         host=hostname,
     )
 
 
 @app.route('/service')
 def service():
-    response = requests.get('http://{}/response'.format(service_endpoint))
-    return response.json()
+    headers = {
+        "source-system": request.headers.get('source-system'),
+        "loan-type": request.headers.get('loan-type'),
+    }
+    response = requests.get('http://{}/response'.format(service_endpoint), headers=headers)
+    return jsonify(response.json())
 
 
 @app.route('/response')
